127294a95f6e8205894013b2c68100d6a30c73e1
Marcin Sucharski <marcin.sucharski.github@gmail.com>
2017-10-03 19:30:54 +0200

    Use materialize-css to make page look better.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('../node_modules/materialize-css/dist/js/materialize');
 require('./index.html');
 require('./style.scss');
 require('./qrcode.ts');
--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,56 @@ const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const extractSass = new ExtractTextPlugin({
   filename: "css/style.css",
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development"
 });
 const extractCSS = new ExtractTextPlugin({
   filename: "css/style.raw.css",
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development"
 });
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist/'),
-    filename: 'js/bundle.js',
+    filename: 'js/bundle.js'
   },
   module: {
     rules: [{
       test: /\.html$/,
       use: 'file-loader?name=[name].[ext]',
-      exclude: /node_modules/,
+      exclude: /node_modules/
     }, {
       test: /\.ts$/,
       use: 'ts-loader',
-      exclude: /node_modules/,
+      exclude: /node_modules/
     }, {
       test: /\.scss$/,
       use: extractSass.extract({
         use: [
           "css-loader",
-          "sass-loader",
+          "sass-loader"
         ],
-        fallback: "style-loader",
-      }),
+        fallback: "style-loader"
+      })
     }, {
       test: /\.css$/,
       use: extractCSS.extract({
         use: 'css-loader',
-        fallback: 'style-loader',
-      }),
-    }],
+        fallback: 'style-loader'
+      })
+    }, {
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [
+          "file-loader?name=fonts/[name].[ext]"
+        ]
+    }]
   },
   devServer: {
-    contentBase: 'dist/',
+    contentBase: 'dist/'
   },
   devtool: 'source-map',
   plugins: [
     extractSass,
-    extractCSS,
-  ],
+    extractCSS
+  ]
 };
